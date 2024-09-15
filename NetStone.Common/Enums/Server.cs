3057e6f1966130015633ae4e8b840dd8ebfbbc73
Tawmy <git@tawmy.dev>
2024-09-15 11:15:40 +0200

    Add server enum

---
+++ NetStone.Common/Enums/Server.cs
@@ -0,0 +1,111 @@
+namespace NetStone.Common.Enums;
+
+public enum Server
+{
+    // Chaos
+    Cerberus,
+    Louisoix,
+    Moogle,
+    Omega,
+    Phantom,
+    Ragnarok,
+    Sagittarius,
+    Spriggan,
+
+    // Light
+    Alpha,
+    Lich,
+    Odin,
+    Phoenix,
+    Raiden,
+    Shiva,
+    Twintania,
+    Zodiark,
+
+    // Materia
+    Bismarck,
+    Ravana,
+    Sephirot,
+    Sophia,
+    Zurvan,
+
+    // Aether
+    Adamantoise,
+    Cactuar,
+    Faerie,
+    Gilgamesh,
+    Jenova,
+    Midgardsormr,
+    Sargatanas,
+    Siren,
+
+    // Crystal
+    Balmung,
+    Brynhildr,
+    Coeurl,
+    Diabolos,
+    Goblin,
+    Malboro,
+    Mateus,
+    Zalera,
+
+    // Primal
+    Behemoth,
+    Excalibur,
+    Exodus,
+    Famfrit,
+    Hyperion,
+    Lamia,
+    Leviathan,
+    Ultros,
+
+    // Dynamis
+    Cuchulainn,
+    Golem,
+    Halicarnassus,
+    Kraken,
+    Maduin,
+    Marilith,
+    Rafflesia,
+    Seraph,
+
+    // Elemental
+    Aegis,
+    Atomos,
+    Carbuncle,
+    Garuda,
+    Gungnir,
+    Kujata,
+    Tonberry,
+    Typhon,
+
+    // Gaia
+    Alexander,
+    Bahamut,
+    Durandal,
+    Fenrir,
+    Ifrit,
+    Ridill,
+    Tiamat,
+    Ultima,
+
+    // Mana
+    Anima,
+    Asura,
+    Chocobo,
+    Hades,
+    Ixion,
+    Masamune,
+    Pandaemonium,
+    Titan,
+
+    // Meteor
+    Belias,
+    Mandragora,
+    Ramuh,
+    Shinryu,
+    Unicorn,
+    Valefor,
+    Yojimbo,
+    Zeromus
+}
