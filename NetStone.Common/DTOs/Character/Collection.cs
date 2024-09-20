9b85b671c60aa465b2c4d306eb6dd2af420e7480
Tawmy <git@tawmy.dev>
2024-09-20 16:23:10 +0200

    Fix CollectionDto parsing

--- NetStone.Common/DTOs/Character/Collection.cs
+++
@@ -1,6 +0,0 @@
-namespace NetStone.Common.DTOs.Character;
-
-public readonly record struct Collection(int Total, int Collected)
-{
-    public decimal CollectedPercentage => Math.Round(decimal.Multiply(decimal.Divide(Collected, Total), 100), 2);
-}
--- NetStone.Common/DTOs/Character/CollectionDto.cs
+++ NetStone.Common/DTOs/Character/CollectionDto.cs
@@ -1,11 +1,7 @@
 namespace NetStone.Common.DTOs.Character;
 
-public record CollectionDto<T>(
-    ICollection<T> List,
-    bool Cached,
-    DateTime? LastUpdated,
-    int Total)
+public record CollectionDto<T>(ICollection<T> List, bool Cached, DateTime? LastUpdated, int Total)
 {
-    private int Total { get; } = Total;
-    public Collection Collection => new(Total, List.Count);
+    public decimal Collected => List.Count;
+    public decimal CollectedPercentage => Math.Round(decimal.Multiply(decimal.Divide(Collected, Total), 100), 2);
 }
