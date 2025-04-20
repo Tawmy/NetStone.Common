bc5a1fee46a845bfe1b75ba7dc7112e805201dd2
Tawmy <git@tawmy.dev>
2025-04-20 13:43:10 +0200

    Remove VersionExtension

--- NetStone.Common/Extensions/VersionExtension.cs
+++
@@ -1,9 +0,0 @@
-namespace NetStone.Common.Extensions;
-
-public static class VersionExtension
-{
-    public static string ToVersionString(this Version version)
-    {
-        return $"{version.Major}.{version.Minor}.{version.Build}";
-    }
-}
