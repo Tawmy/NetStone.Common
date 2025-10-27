73fe5d3b214c2f66ce8025ed5e60de57a071196c
Tawmy <git@tawmy.dev>
2025-10-27 18:54:01 +0100

    Add short names to ClassJob enum

--- NetStone.Common/Enums/ClassJob.cs
+++ NetStone.Common/Enums/ClassJob.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace NetStone.Common.Enums;
@@ -7,73 +8,73 @@ public enum ClassJob
 {
     #region A Realm Reborn
 
-    Gladiator,
-    Pugilist,
-    Marauder,
-    Lancer,
-    Archer,
-    Conjurer,
-    Thaumaturge,
-    Carpenter,
-    Blacksmith,
-    Armorer,
-    Goldsmith,
-    Leatherworker,
-    Weaver,
-    Alchemist,
-    Culinarian,
-    Miner,
-    Botanist,
-    Fisher,
-    Paladin,
-    Monk,
-    Warrior,
-    Dragoon,
-    Bard,
-    WhiteMage,
-    BlackMage,
-    Arcanist,
-    Summoner,
-    Scholar,
-    Rogue,
-    Ninja,
+    [Display(ShortName = "GLA")] Gladiator,
+    [Display(ShortName = "PGL")] Pugilist,
+    [Display(ShortName = "MRD")] Marauder,
+    [Display(ShortName = "LNC")] Lancer,
+    [Display(ShortName = "ARC")] Archer,
+    [Display(ShortName = "CNJ")] Conjurer,
+    [Display(ShortName = "THM")] Thaumaturge,
+    [Display(ShortName = "CRP")] Carpenter,
+    [Display(ShortName = "BSM")] Blacksmith,
+    [Display(ShortName = "ARM")] Armorer,
+    [Display(ShortName = "GSM")] Goldsmith,
+    [Display(ShortName = "LTW")] Leatherworker,
+    [Display(ShortName = "WVR")] Weaver,
+    [Display(ShortName = "ALC")] Alchemist,
+    [Display(ShortName = "CUL")] Culinarian,
+    [Display(ShortName = "MIN")] Miner,
+    [Display(ShortName = "BTN")] Botanist,
+    [Display(ShortName = "FSH")] Fisher,
+    [Display(ShortName = "PLD")] Paladin,
+    [Display(ShortName = "MNK")] Monk,
+    [Display(ShortName = "WAR")] Warrior,
+    [Display(ShortName = "DRG")] Dragoon,
+    [Display(ShortName = "BRD")] Bard,
+    [Display(ShortName = "WHM")] WhiteMage,
+    [Display(ShortName = "BLM")] BlackMage,
+    [Display(ShortName = "ACN")] Arcanist,
+    [Display(ShortName = "SMN")] Summoner,
+    [Display(ShortName = "SCH")] Scholar,
+    [Display(ShortName = "ROG")] Rogue,
+    [Display(ShortName = "NIN")] Ninja,
 
     #endregion
 
     #region Heavensward
 
-    Machinist,
-    DarkKnight,
-    Astrologian,
+    [Display(ShortName = "MCH")] Machinist,
+    [Display(ShortName = "DRK")] DarkKnight,
+    [Display(ShortName = "AST")] Astrologian,
 
     #endregion
 
     #region Stormblood
 
-    Samurai,
-    RedMage,
-    BlueMage,
+    [Display(ShortName = "SAM")] Samurai,
+    [Display(ShortName = "RDM")] RedMage,
+    [Display(ShortName = "BLU")] BlueMage,
 
     #endregion
 
     #region Shadowbringers
 
-    Gunbreaker,
-    Dancer,
+    [Display(ShortName = "GNB")] Gunbreaker,
+    [Display(ShortName = "DNC")] Dancer,
 
     #endregion
 
     #region Endwalker
 
-    Reaper,
-    Sage,
+    [Display(ShortName = "RPR")] Reaper,
+    [Display(ShortName = "SGE")] Sage,
 
     #endregion
 
     #region Dawntrail
 
-    Viper,
-    Pictomancer
+    [Display(ShortName = "VPR")] Viper,
+    [Display(ShortName = "PCT")] Pictomancer
 
     #endregion
 }
