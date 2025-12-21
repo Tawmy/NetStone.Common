b5744a862c4066c79db4d79cb00b256859fbf7d4
Tawmy <git@tawmy.dev>
2025-12-21 10:25:35 +0100

    Turn FallbackType into a flags enum, add values

--- NetStone.Common/Enums/FallbackType.cs
+++ NetStone.Common/Enums/FallbackType.cs
@@ -1,19 +1,30 @@
 namespace NetStone.Common.Enums;
 
+[Flags]
 public enum FallbackType
 {
     /// <summary>
     ///     Never use fallback.
     /// </summary>
-    None,
+    None = 0,
 
     /// <summary>
     ///     Use fallback for <see cref="HttpRequestException" />.
     /// </summary>
-    Http,
+    Http = 1 << 0,
+
+    /// <summary>
+    ///     Use fallback when Lodestone is unavailable, either for maintenance or if profile sub-pages are private.
+    /// </summary>
+    LodestoneUnavailable = 1 << 1,
+
+    /// <summary>
+    ///     Use fallback when profile is private. This only works for a user's full profile, not sub-pages like minions!
+    /// </summary>
+    ProfilePrivate = 1 << 2,
 
     /// <summary>
     ///     Use fallback for any <see cref="Exception" />.
     /// </summary>
-    Any
+    Any = 1 << 3
 }
